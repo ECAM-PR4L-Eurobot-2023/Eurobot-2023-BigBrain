99072beec4b7c35eb799f245ad4d82a76de7ae5f
alex210501 <alejandrobrb210501@gmail.com>
2023-02-18 11:31:16 +0000

    (#1) Add RosApi to communicate using ROS

--- src/ros_api/ros_api.py
+++ src/ros_api/ros_api.py
@@ -6,6 +6,7 @@ from flash_mcqueen_api import FlashMcQueenApi
 NODE_NAME = "bigbrain"
 
 class RosApi:
+    """ API for ROS communication """
     def __init__(self):
         self.flash_mcqueen = FlashMcQueenApi()
 
@@ -13,5 +14,18 @@ class RosApi:
         rospy.init_node(NODE_NAME)
 
 if __name__ == '__main__':
+    import time
+
+    from flash_mcqueen_api import Position, PidParameters
+
     while 1:
+        position = Position(1, 2, 3.0)
+        pid_param = PidParameters(5, 6, 7)
+
         ros_api = RosApi()
+        ros_api.flash_mcqueen.set_position(position)
+        ros_api.flash_mcqueen.set_rotation(90)
+        ros_api.flash_mcqueen.set_pid_angle(pid_param)
+
+        time.sleep(2)
+        
